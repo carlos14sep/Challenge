5c112bb2e19e6b707127afa6c3f7236903c5a903
carlos <cmmgallego@hotmail.com>
2020-08-25 10:07:35 -0500

    Reto 1

---
+++ App.1.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+export class App extends Component {
+    render() {
+        <div>
+            <h1>Hellos mundo</h1>
+            <input type="text" placeholder="" />
+        </div>;
+    }
+}
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import "./App.css"
+function App() {
+ 
+  const [input, setInput] = useState("");
+  
+  return (
+    <div class="App-header">
+      <p>Digite su nombre: {input} </p>
+      <div class= "App-input">
+        <input class ="App-input" value={input} onInput={e => setInput(e.target.value)}/>
+      </div>
+    </div>
+    
+  );
+ 
+}
+export default App
