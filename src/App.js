cd266d8b0694abcdc8d4b4aedf5fdd4e3eca7983
carlos <cmmgallego@hotmail.com>
2020-08-30 15:25:12 -0500

    Reto 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { useState } from 'react';
-import Challenger1 from "../src/Challenger1.js"
-import Challenge2 from "../src/Challenger2.js"
-import PersonList from '../src/PersonList.js';
+import RetoOne from "./RetoOne.js"
+import RetoTwo from './RetoTwo.js';
 
 function Reto (){
   return(
     <div>
-      <Challenger1/>
-      {/* <Challenge2/> */}
-      <PersonList/>
+      <RetoOne/>
+      <RetoTwo/>
     </div>
   )
 }
