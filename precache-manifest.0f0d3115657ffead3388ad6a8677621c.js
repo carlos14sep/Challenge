724e01ebc2c0c3ca6808a119778b1424fe888fb5
carlos <cmmgallego@hotmail.com>
2020-09-04 09:51:52 -0500

    Updates

---
+++ precache-manifest.0f0d3115657ffead3388ad6a8677621c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dcd8f8b48c2d1ea4c43324de7bc33e0",
+    "url": "/Challenge/index.html"
+  },
+  {
+    "revision": "a71532fcdb18f617890b",
+    "url": "/Challenge/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "427be6bcda3150287147",
+    "url": "/Challenge/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a71532fcdb18f617890b",
+    "url": "/Challenge/static/js/2.53c1c8b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Challenge/static/js/2.53c1c8b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "427be6bcda3150287147",
+    "url": "/Challenge/static/js/main.77dad9de.chunk.js"
+  },
+  {
+    "revision": "97c52d5eba73d6d44197",
+    "url": "/Challenge/static/js/runtime-main.95ef9be4.js"
+  }
+]);
