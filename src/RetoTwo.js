989fa828abf8c995ce491a32d27a9fcc2e153001
carlos <cmmgallego@hotmail.com>
2020-09-04 09:44:15 -0500

    Actualizacion

--- src/RetoTwo.js
+++ src/RetoTwo.js
@@ -1,57 +1,57 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 
-function PersonList (){
+function PersonList() {
     const [covidCases, setCovidCases] = useState([]);
     const [city, setCity] = useState("");
     const [date, setDate] = useState("");
-    const getCovid =() => {
+    const getCovid = () => {
         axios
-        .get("https://www.datos.gov.co/resource/gt2j-8ykr.json?ciudad_de_ubicaci_n="+city+"&fecha_diagnostico="+ date +"T00:00:00.000" )
-        .then((res)=>{
-            setCovidCases(res.data)
-        })
+            .get("https://www.datos.gov.co/resource/gt2j-8ykr.json?ciudad_de_ubicaci_n=" + city + "&fecha_diagnostico=" + date + "T00:00:00.000")
+            .then((res) => {
+                setCovidCases(res.data)
+            })
     }
-    return(
+    return (
         <div className="Container">
             <div className="row">
                 <div className="col">
-                <h3 className="my-4 text-center">Consulta sobre pacientes con Covid-19 en el Oriente Antioqueño</h3>
-                <div className="row">
-                    <div className="col-3 offset-2">
-                        <input className ="form-control" placeholder="Municipio" type="text" value={city} onInput={e => setCity(e.target.value)}/>  
+                    <h3 className="my-4 text-center">Consulta sobre pacientes con Covid-19 en el Oriente Antioqueño</h3>
+                    <div className="row">
+                        <div className="col-3 offset-2">
+                            <input className="form-control" placeholder="Municipio" type="text" value={city} onInput={e => setCity(e.target.value)} />
+                        </div>
+                        <div className="col-3">
+                            <input className="form-control" type="date" value={date} onInput={d => setDate(d.target.value)} />
+                        </div>
+                        <div className="col-3">
+                            <button className="btn btn-secondary" onClick={getCovid}>Search</button>
+                        </div>
                     </div>
-                    <div className="col-3">
-                        <input className ="form-control" type = "date" value={date} onInput={d => setDate(d.target.value)}/>
-                    </div>
-                    <div className="col-3">
-                        <button className = "btn btn-secondary" onClick={getCovid}>Search</button>
-                    </div>
+                    <table className="table table-bordered table-dark mt-5" >
+                        <thead className="thead-dark">
+                            <tr>
+                                <th scope="col-3 right">Edad</th>
+                                <th scope="col">Sexo</th>
+                                <th scope="col">Tipo</th>
+                                <th scope="col">Estado</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {covidCases.map((covidCases, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td >{covidCases.edad}</td>
+                                        <td >{covidCases.sexo}</td>
+                                        <td >{covidCases.tipo}</td>
+                                        <td >{covidCases.estado}</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
                 </div>
-                <table className="table table-bordered table-dark mt-5" >
-                    <thead className="thead-dark">
-                        <tr>
-                            <th scope="col-3 right">Edad</th>
-                            <th scope="col">Sexo</th>
-                            <th scope="col">Tipo</th>
-                            <th scope="col">Estado</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {covidCases.map((covidCases, index) => {
-                            return(
-                                <tr key={index}>
-                                    <td >{covidCases.edad}</td>
-                                    <td >{covidCases.sexo}</td>
-                                    <td >{covidCases.tipo}</td>
-                                    <td >{covidCases.estado}</td>
-                                </tr>
-                            )
-                        })} 
-                    </tbody>
-                </table>
             </div>
-        </div>
-    </div>)
+        </div>)
 }
 export default PersonList
